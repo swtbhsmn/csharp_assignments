eca6b35108cb8bb26f53805e87bd995e93ca95e0
swetabh suman <swtbhsmn@gmail.com>
2021-08-09 17:24:42 +0530

    json_file_2_List

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -12,6 +12,7 @@ namespace Assginments
                 Console.Write("Type 1: For Word Frequency Problem.\n" +
                               "Type 2: For Calculate Date of Birth.\n" +
                               "Type 3: For Remove Words From Text.\n" +
+                              "Type 4: For Json to List\n" +
                               "Enter Your Choice: ");
 
                 choice = Convert.ToInt32(Console.ReadLine());
@@ -27,6 +28,9 @@ namespace Assginments
                         break;
                     case 3:
                         StopWordRemove.StopWordRemoveCaller();
+                        break;
+                    case 4:
+                        JsonToList.GetJsonFile("C:\\Datagrokr\\c#\\Assignments\\Assignments\\Share\\data.json");
                         break;
                     default:
                         Console.WriteLine("Invalid Choice!");
